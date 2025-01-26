3e620a73c3a40b6e6fdc84d5ca2303ca0d91af79
Bernst4315 <bernst4315@gmail.com>
2025-01-26 11:01:52 -0700

    homepage loads

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 const port = 8080; 
 
 app.listen(port, () => {
     console.log("server is running");
+})
+
+//middleware
+
+app.use(express.static("public"))
+
+//Website GET requests
+
+app.get("/", (req,res) =>{
+    res.sendFile("./views/home.html", {root: __dirname})
+})
+
+app.get("/about", (req,res) =>{
+    res.sendFile("./views/about.html", {root: __dirname})
 })
