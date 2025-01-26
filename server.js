67ea2299d4c482a1638b0e5436471e252e137b66
Bernst4315 <bernst4315@gmail.com>
2025-01-26 12:01:22 -0700

    added ejs functionalit

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const fs = require("fs");
 
-const app = express();
 const port = 8080; 
+
+const app = express();
+
+app.set("view engine", "ejs");
+
 
 app.listen(port, () => {
     console.log("server is running");
@@ -10,14 +14,18 @@ app.listen(port, () => {
 
 //middleware
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 //Website GET requests
 
 app.get("/", (req,res) =>{
-    res.sendFile("./views/home.html", {root: __dirname})
+    res.render("home");
 })
 
 app.get("/about", (req,res) =>{
-    res.sendFile("./views/about.html", {root: __dirname})
+    res.render("about");
+})
+
+app.get("/Survey", (req,res) => {
+    res.render("Survey")
 })
